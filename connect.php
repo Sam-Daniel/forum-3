5b9982b2e5d9a5e46283ca6fc7ba140c623f5d73
ddany <duminydaniel@gmail.com>
2018-01-29 00:30:42 +0530

    Update connect.php

--- connect.php
+++ connect.php
@@ -25,7 +25,7 @@ $DB_HOST = "";
 $DB_USER = "";
 $DB_PASS = "";
 global $connection;
-$connection = mysql_connect($DB_HOST, $DB_USER, $DB_PASS)
+$connection = mysqli_connect($DB_HOST, $DB_USER, $DB_PASS)
 or die("Cannot connect to $DB_HOST as $DB_USER:" . mysql_error());
 mysql_select_db($DB_NAME) or die ("Cannot open $DB_NAME:" . mysql_error());
 return $connection;
